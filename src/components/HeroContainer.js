07929d2efdd01d91d2628917e1a9192a92e18a58
Dan Chung <danetix@gmail.com>
2018-11-29 12:58:50 -0500

    added hero samples

--- src/components/HeroContainer.js
+++ src/components/HeroContainer.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 export default class HeroContainer extends Component {
+  state = {
+    images: [
+      {
+        url: '/img/hero_1.jpg',
+        title: 'Hero Image 1',
+        content: 'Hero Content'
+      },
+      {
+        url: '/img/hero_2.jpg',
+        title: 'Hero Image 2',
+        content: 'Hero Content'
+      },
+      {
+        url: '/img/hero_3.jpg',
+        title: 'Hero Image 3',
+        content: 'Hero Content'
+      }
+    ]
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div id="hero-wrap">HeroContainer</div>
+        <div id="hero-wrap">
+          <img src={this.state.images[1].url} />
+        </div>
       </React.Fragment>
     )
   }
