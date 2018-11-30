9a48e1f9769d90109c588dd65c828d5ab13791c4
Dan Chung <danetix@gmail.com>
2018-11-29 22:14:48 -0500

    post styling

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react'
 
 export default class BodyContainer extends Component {
+  state = {
+    posts: [
+      {
+        title: "Bureaux exquisite delightful carefully curated soft power.",
+        author: "LOREM IPSUM",
+        date: "FEBRUARY 10, 2017",
+        imgUrl: "/img/content_1.jpg" 
+      },
+      {
+        title: "Esse airport veniam ryokan soft power.",
+        author: "LOREM IPSUM",
+        date: "JANUARY 21 2017",
+        imgUrl: "/img/content_1.jpg" 
+      },
+      {
+        title: "Sharp bureaux sleepy K-pop carefully curated.",
+        author: "LOREM IPSUM",
+        date: "FEBRUARY 2 2017",
+        imgUrl: "/img/content_1.jpg" 
+      }
+    ]
+  }
+
+  renderPosts = () => {
+    return this.state.posts.map(post => {
+      return (
+        <div className="card-wrap">
+          <img src={post.imgUrl} className="featured-img" />
+          <p className="date">{post.date}</p>
+          <h1 className="title">{post.title}</h1>
+          <p className="author">{post.author}</p>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <div id="body-wrap">
           <div className="col">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur urna nibh, vel luctus lorem eleifend eu. Vestibulum pellentesque nisi at enim venenatis interdum. Aliquam ac augue sit amet nunc aliquam accumsan. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur rutrum est in erat faucibus, eu dignissim lectus malesuada. Proin semper, nibh quis condimentum malesuada, eros nunc pretium enim, vitae cursus urna orci sit amet nibh. Nunc malesuada erat eget dictum finibus. Cras enim metus, vulputate ut odio sit amet, tristique elementum dolor. Phasellus pharetra vitae velit nec tempus.</p>
-            <p>Pellentesque id sem eu diam consequat congue ut varius lorem. Donec eget auctor ipsum. Phasellus et quam eu purus lobortis pretium. Pellentesque dapibus at lacus a posuere. Proin tincidunt nibh quam, nec rutrum dolor suscipit id. Nam ac ipsum eu arcu tempus vestibulum. Phasellus pharetra libero tempus convallis iaculis. Fusce sit amet diam elit. Aliquam tempus cursus orci, in cursus massa rhoncus eu. Nulla sed eleifend enim. Donec dictum semper risus, vel ultricies orci bibendum in. Curabitur et tellus sit amet elit molestie semper. Pellentesque ac accumsan turpis.</p>
+            {this.renderPosts()}
           </div>
+          <div className="col"></div>
           <div className="col">
-            <p>In auctor mauris mauris, vitae feugiat turpis consectetur eu. Nunc luctus, lectus non tempor vulputate, diam mauris vestibulum erat, in convallis dolor lectus sed metus. Nullam eleifend vitae tellus id porta. Aliquam posuere quam et eros porta, at mattis ex facilisis. Vivamus metus velit, rhoncus a magna vel, tincidunt semper felis. Donec vel elementum felis. Curabitur blandit, justo in interdum dictum, turpis augue pretium odio, vel ultricies odio nulla id turpis. Nam vestibulum euismod mi tristique commodo. Duis eu lacus diam.</p>
-            <p>In vehicula odio fringilla metus molestie tempor. Donec magna quam, congue at gravida quis, mollis vitae felis. Etiam a lorem ut purus iaculis pretium bibendum quis ligula. Curabitur tincidunt, felis ac sollicitudin fermentum, velit massa bibendum libero, dignissim interdum lectus ante a tellus. Vestibulum vel libero eu massa tempor commodo. Aenean aliquet pulvinar lacus, eget tristique felis porta vel. Etiam nibh est, porta eu laoreet in, malesuada a ipsum. Vestibulum ac elementum sapien, eget convallis nunc. Vestibulum fermentum libero eu porttitor congue. Nullam vestibulum mi ex. In venenatis ultrices mi sed congue. Morbi dignissim varius elit a iaculis. Suspendisse sit amet nulla nec nibh auctor accumsan. In vitae lacus et lacus congue euismod. In elementum egestas justo eu maximus. Ut rutrum, dolor vitae pellentesque bibendum, metus nisl egestas quam, et ultricies orci enim et ante.</p>
+            {this.renderPosts()}
           </div>
+          <div className="col"></div>
           <div className="col">
-            <p>Suspendisse ut efficitur justo, id auctor mi. Sed dui mauris, sagittis quis nunc at, iaculis laoreet neque. In hac habitasse platea dictumst. Nullam lacinia metus eget pellentesque rutrum. Morbi egestas mi mollis, lacinia mauris ut, tristique nisi. Nulla sed sagittis ante. Suspendisse potenti. Pellentesque commodo, tellus ac feugiat sagittis, dui arcu mattis tortor, in dignissim velit massa auctor odio. Ut libero urna, cursus vel felis vel, pellentesque suscipit lorem. Maecenas vulputate egestas nulla. Nulla eros diam, placerat vel lacus vel, sodales varius erat. Nulla ornare nunc eu congue suscipit. Cras lacus lorem, mattis in posuere ut, interdum tempus sapien.</p>
+            {this.renderPosts()}
           </div>
         </div>
       </React.Fragment>
