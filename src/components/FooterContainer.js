7cdb056e975b9bae1f395f6e2d8761e613b2862e
Dan Chung <danetix@gmail.com>
2018-12-03 01:35:55 -0500

    more styling

--- src/components/FooterContainer.js
+++ src/components/FooterContainer.js
@@ -4,7 +4,9 @@ export default class FooterContainer extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="footer-wrap"></div>
+        <div id="footer-wrap">
+          <p>© 2017 — SPARTA PLAESENT - <a href="#">instagram</a> - <a href="#">facebook</a> - <a href="#">twitter</a></p>
+        </div>
       </React.Fragment>
     )
   }
